5e51573ecf31abc3122d84fa8fa2c443d6c76d47
WEI <493559615@qq.com>
2019-05-15 09:58:21 +0800

    section8

---
+++ section8/8.1.cpp
@@ -0,0 +1,24 @@
+/**
+函数返回类型为istream&
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+istream &Func(istream &istr)
+{
+	char ch;
+	while (istr >> ch)
+		cout << ch;
+	cout << endl;
+	istr.clear();
+	return istr;
+}
+
+int main(void)
+{
+	if (Func(cin))
+		cout << "Func() is ok!" << endl;
+	return 0;
+}
---
+++ section8/8.2.cpp
@@ -0,0 +1,26 @@
+/**
+函数返回类型为istream&
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+istream &Func(istream &istr)
+{
+	cout << std::unitbuf;
+	char ch;
+	while (istr >> ch)
+		cout << ch;
+	cout << endl;
+	istr.clear();
+	cout << std::nounitbuf;
+	return istr;
+}
+
+int main(void)
+{
+	if (Func(cin))
+		cout << "Func() is ok!" << endl;
+	return 0;
+}
